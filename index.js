6a3949691898003d109ad97d0166b30a83d414f4
Amiel Martin <amiel@actionsprout.com>
2017-06-19 08:05:15 -0700

    Export UriTemplate as uri-templates

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-uri-templates-shim'
+  name: 'ember-cli-uri-templates-shim',
+
+  options: {
+    nodeAssets: {
+      'uri-templates': {
+        import: ['uri-templates.js']
+      }
+    }
+  }
 };
